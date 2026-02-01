6c173173240dc77977b43fe2f0390e3a49e4441e
sridevijagadesh <sridevi.j2719@gmail.com>
2026-02-01 23:01:32 +0530

    Eposide 1

---
+++ index.js
@@ -0,0 +1,21 @@
+// {
+//   /* <div className="parent">
+//   <div className="child">
+//     <h1>i am H!</h1>
+//   </div>
+// </div>; */
+// }
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am inside H1"),
+    React.createElement("h2", {}, "i am heading 2"),
+  ]),
+);
+
+// const heading = React.createElement("h1", null, "i am from react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
